488c3d92a31ecb98502c9d3cc88161ad212f4df5
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-04-14 23:06:44 -0300

    first commit

---
+++ src/main/java/br/com/fiap/chapecos/ChapecosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.chapecos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapecosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/chapecos/ChapecosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.chapecos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapecosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
