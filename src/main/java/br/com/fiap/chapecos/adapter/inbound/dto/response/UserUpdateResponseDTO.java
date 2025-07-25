e0909eeac8eb42a37f7d0bef16b3640c1b0bd365
Francisco Aurizelio <franads@gmail.com>
2025-07-25 12:23:37 -0300

    feat: refactora estrutura para aplicar Clean Architere

---
+++ src/main/java/br/com/fiap/chapecos/adapter/inbound/dto/response/UserUpdateResponseDTO.java
@@ -0,0 +1,31 @@
+package br.com.fiap.chapecos.dto.response;
+
+import br.com.fiap.chapecos.domain.model.Role;
+import br.com.fiap.chapecos.domain.model.User;
+
+public record UserUpdateResponseDTO(
+
+        Long id,
+
+        String email,
+
+        String userName,
+
+        AddressResponseDTO address,
+
+        Role role,
+
+        AuditUpdateAsResponseDTO audit
+) {
+
+    public UserUpdateResponseDTO(User user) {
+        this(
+                user.getId(),
+                user.getEmail(),
+                user.getUsername(),
+                new AddressResponseDTO(user.getAddress()),
+                user.getRole(),
+                new AuditUpdateAsResponseDTO(user.getAudit().getUpdateAs())
+        );
+    }
+}
